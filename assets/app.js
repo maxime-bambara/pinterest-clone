f5c7c3452e20445058ff15e95e6de7b9d0981487
maxime bambara <maximebambara@iMac-de-maxime.lan>
2020-12-21 09:09:38 +0100

    Fix issue with file name not showing after image selection

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,7 @@ import './scss/app.scss';
 import $ from 'jquery';
 import 'bootstrap';
 
+$('.custom-file-input').on('change', function(e) {
+    let inputFile = e.currentTarget;
+    $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+});

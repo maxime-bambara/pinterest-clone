a9e3823f83f9915fe3b4ee0e61183954c1afb9ca
maxime bambara <maxime.f.bambara@gmail.com>
2021-01-07 11:30:55 +0100

    Add edit profile page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +14,7 @@ class AccountController extends AbstractController
     /**
      * @Route("/account", name="app_account")
      */
-    public function index(): Response
+    public function profile(): Response
     {
         if(!$this->getUser()){
             return $this->redirectToRoute('app_login');
@@ -19,6 +22,35 @@ class AccountController extends AbstractController
 
         return $this->render('account/profile.html.twig', [
             'controller_name' => 'AccountController',
+        ]);
+    }
+
+    /**
+     * @Route("/account/edit", name="app_account_edit")
+     */
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        };
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()&&$form->isValid()){
+            $em->flush();
+
+            $this->addFlash('success', 'Account updated successfully');
+
+            return $this->redirectToRoute('app_account');
+
+
+        }
+
+        return $this->render('account/profile_edit.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }

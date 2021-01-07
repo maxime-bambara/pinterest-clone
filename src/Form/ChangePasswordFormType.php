ab40ec28f230338e67bd81c3d7fd4f4b27408a46
maxime bambara <maxime.f.bambara@gmail.com>
2021-01-07 12:42:21 +0100

    Add current password in change password page

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -2,11 +2,13 @@
 
 namespace App\Form;
 
+use phpDocumentor\Reflection\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -14,6 +16,19 @@ class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        if ($options['current_password_is_required']){
+        $builder
+            ->add('currentPassword', PasswordType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your current password'
+                    ]),
+                    new UserPassword([
+                        'message' => 'Current password invalid'
+                    ]),
+                ]
+            ]);
+        };
         $builder
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
@@ -44,6 +59,8 @@ class ChangePasswordFormType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([]);
+        $resolver->setDefaults([
+            'current_password_is_required' => false
+        ]);
     }
 }

7a13d3deaccf5cd39d4492132dd3836a8f2b9807
maxime bambara <maxime.f.bambara@gmail.com>
2021-01-07 12:11:11 +0100

    Add change password feature

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordFormType;
 use App\Form\UserFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
@@ -53,4 +56,35 @@ class AccountController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/account/change-password", name="app_account_change_password")
+     */
+    public function changePassword(Request $request, EntityManagerInterface $em,
+    UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        };
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(ChangePasswordFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&&$form->isValid()){
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $form['plainPassword']->getData()));
+            $em->flush();
+
+            $this->addFlash('success', 'Password updated successfully');
+
+            return $this->redirectToRoute('app_account');
+        };
+
+        return $this->render('account/profile_change_password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }

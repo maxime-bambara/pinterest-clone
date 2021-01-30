bb07f3e720aca1e7e763ef5e88a95692d00e42b8
maxime bambara <maxime.f.bambara@gmail.com>
2021-01-30 17:11:23 +0100

    Ajout des fixtures (temporaire)

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];

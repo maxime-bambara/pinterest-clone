2ee1942701f776513e1674987d5f19eae061828c
maxime bambara <maxime.f.bambara@gmail.com>
2021-01-30 15:28:31 +0100

    Fix database structure

--- migrations/Version20210106155658.php
+++ migrations/Version20210106155658.php
@@ -14,7 +14,7 @@ final class Version20210106155658 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Cteate reset_password_requests table';
+        return 'Create reset_password_requests table';
     }
 
     public function up(Schema $schema) : void
